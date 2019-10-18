ba5f636f264a096c798e2346faac9b7a5a9a7d6e
Jared Gray <jaredrgray@outlook.com>
2019-10-18 06:33:08 -0700

    Added basic model shapes

---
+++ src/main/java/org/tesla/interview/model/AggregateSample.java
@@ -0,0 +1,39 @@
+package org.tesla.interview.model;
+
+/**
+ * An aggregation of several {@link MeasurementSample} instances.
+ */
+public class AggregateSample {
+
+  private final long timestamp;
+
+  private final String id;
+
+  private final int aggregateValue;
+
+  /**
+   * Constructor.
+   * 
+   * @param timestamp number of milliseconds since January 1, 1970:UTC
+   * @param id unique identifier for the asset
+   * @param aggregateValue aggregation value of constituent samples
+   */
+  public AggregateSample(long timestamp, String id, int aggregateValue) {
+    this.timestamp = timestamp;
+    this.id = id;
+    this.aggregateValue = aggregateValue;
+  }
+
+  public int getAggregateValue() {
+    return aggregateValue;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public long getTimestamp() {
+    return timestamp;
+  }
+
+}
---
+++ src/main/java/org/tesla/interview/model/IntegerHashtag.java
@@ -0,0 +1,36 @@
+package org.tesla.interview.model;
+
+public enum IntegerHashtag {
+
+  /* empty comments present below to please the code formatter */
+  ONE("#one", 1), //
+  TWO("#two", 2), //
+  THREE("#three", 3), //
+  FOUR("#four", 4), //
+  FIVE("#five", 5), //
+  SIX("#six", 6), //
+  SEVEN("#seven", 7), //
+  EIGHT("#eight", 8), //
+  NINE("#nine", 9), //
+  TEN("#ten", 10) //
+  ;
+
+  /** string representation of the hashtag. */
+  private final String tag;
+
+  /** integer representation of the hashtag's value. */
+  private final int value;
+
+  private IntegerHashtag(String tag, int value) {
+    this.tag = tag;
+    this.value = value;
+  }
+
+  public String getTag() {
+    return this.tag;
+  }
+
+  public int getValue() {
+    return this.value;
+  }
+}
